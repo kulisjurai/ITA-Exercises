c9c2aa3caaebac5b7e62ca71c06c3a5eedd8cde3
jurakulis21 <71762114+jurakulis21@users.noreply.github.com>
2021-03-18 23:19:30 +0100

    up

--- treeStructureAngular/script.js
+++ treeStructureAngular/script.js
@@ -4,9 +4,9 @@ const myApp = angular.module('myApp', []);
 myApp.controller('MainController', function ($scope, $http) {
 
     $http.get('/tree-data.json').then(function (data) {
-
         $scope.treeData = data.data;
     });
+
     $scope.showFolder = function (children) {
         console.log($scope.treeData)
         if (children === undefined) {
